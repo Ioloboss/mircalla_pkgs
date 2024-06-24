155e6d54e47a125b807cc8e3a98df40ee5f2cf9f
Harrison Jones <harrisoniolojones@gmail.com>
2024-06-24 19:52:35 +0100

    Added missing semicolons

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
 	description = "Mircalla Packages";
 
 	outputs = { self, nixpkgs }: { system }: {
-		status-bar = import ./status-bar.nix { pkgs = nixpkgs { inherit system }; };
-		flashcards = import ./flashcards.nix { pkgs = nixpkgs { inherit system }; };
-		diary = import ./diary.nix { pkgs = nixpkgs { inherit system }; };
-		fhs = import ./fhs.nix { pkgs = nixpkgs { inherit system }; };
+		status-bar = import ./status-bar.nix { pkgs = nixpkgs { inherit system; }; };
+		flashcards = import ./flashcards.nix { pkgs = nixpkgs { inherit system; }; };
+		diary = import ./diary.nix { pkgs = nixpkgs { inherit system; }; };
+		fhs = import ./fhs.nix { pkgs = nixpkgs { inherit system; }; };
 	};
 }
